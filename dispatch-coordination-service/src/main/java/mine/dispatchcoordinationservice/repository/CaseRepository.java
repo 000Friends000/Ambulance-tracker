96ce08d3b668ad5885150e6d01c341abec0678af
mouad-ou <79291830+mouad-ou@users.noreply.github.com>
2025-01-12 22:20:36 +0100

    Delete dispatch-coordination-service directory

--- dispatch-coordination-service/src/main/java/mine/dispatchcoordinationservice/repository/CaseRepository.java
+++
@@ -1,9 +0,0 @@
-package mine.dispatchcoordinationservice.repository;
-
-import mine.dispatchcoordinationservice.model.Case;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CaseRepository extends JpaRepository<Case, Integer> {
-}
